5f30addd39f05a47fceac901e91a09bb4420cf2d
Qibin Liang <46101145+PhyseChan@users.noreply.github.com>
2023-04-19 10:35:10 +0800

    update

--- run_single.sh
+++ run_single.sh
@@ -4,20 +4,22 @@
 # stage 0: preprocess the data
 # stage 1: train the model
 
-stage=-1
-stop_stage=2
+stage=1
+stop_stage=1
 token_level="char"
 batch_size=6
-lr=0.00005
+lr=0.01
+min_lr=0.00001
 epochs=350
 d_model=512
 n_layers=6
 n_heads=8
 dropout=0.1
 d_ff=2048
-accumulation_steps=8
-max_len=5000
+accumulation_steps=16
+max_len=512
 save_interval=4
+verbose_interval=100
 
 if [ ${stage} -le -1 ] && [ ${stop_stage} -ge -1 ]; then
     echo "stage -1: Download the data"
@@ -40,6 +42,7 @@ if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
         --token_level "$token_level" \
         --batch_size "$batch_size" \
         --lr "$lr" \
+        --min_lr "$min_lr" \
         --epochs "$epochs" \
         --d_model "$d_model" \
         --n_layers "$n_layers" \
